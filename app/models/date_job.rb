c1eba587f3138072e9773def6b83c8a0e0657c24
SergiiMelakh <msvkharkov@gmail.com>
2015-02-03 10:19:46 +0200

    Edit all models

--- app/models/date_job.rb
+++ app/models/date_job.rb
@@ -1,5 +1,5 @@
 class DateJob < ActiveRecord::Base
 
-
+  has_many :jobs, dependent: :destroy 
   validates :date, uniqueness: true
 end
--- app/models/street.rb
+++ app/models/street.rb
@@ -1,5 +1,5 @@
 class Street < ActiveRecord::Base
 
-  
+  has_many :jobs, dependent: :destroy 
   validates :street, presence: true
 end
