dca230f81257954af5cc36f18bac82ee58d6a5e7
SergiiMelakh <msvkharkov@gmail.com>
2015-02-04 17:21:25 +0200

    Edit Controller Report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,18 +1,30 @@
 class ReportsController < ApplicationController
-  def filter
 
+  def filter
   end
 
   def index
     @date_start = Date.civil(params[:date_start][:year].to_i, params[:date_start][:month].to_i, params[:date_start][:day].to_i)
     @date_end = Date.civil(params[:date_end][:year].to_i, params[:date_end][:month].to_i, params[:date_end][:day].to_i)
-    
+    @jobs_period = []
     
       if params[:filter][:street_id] == "1"
-        @jobs_period = DateJob.where("date >= ? AND date <= ?", @date_start, @date_end)
-        #@all_jobs = 
+        DateJob.where("date >= ? AND date <= ?", @date_start, @date_end).find_each do |dj|
+          dj.jobs.each do |j|
+           @jobs_period << j 
+          end
+         end
+
       else
-        #@jobs_period = DateJob.where("date >= ? AND date <= ?", @date_start, @date_end)
+        DateJob.where("date >= ? AND date <= ?", @date_start, @date_end).find_each do |dj|
+          dj.jobs.each do |j|
+            if j.street_id == params[:filter][:street_id].to_i
+              @jobs_period << j 
+            end
+          end
+         end
       end
+
+
   end
 end
