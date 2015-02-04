f87cb72fa6f290fa57316f034be10856e6aacc75
SergiiMelakh <msvkharkov@gmail.com>
2015-02-04 09:35:11 +0200

    Add validates for JOB model

--- app/models/date_job.rb
+++ app/models/date_job.rb
@@ -2,5 +2,7 @@ class DateJob < ActiveRecord::Base
 
   has_many :jobs, dependent: :destroy 
   validates :date, uniqueness: true
-  accepts_nested_attributes_for :jobs, allow_destroy: true, reject_if: proc { |a| (a['square'].blank? || a['line_id'] == '1')}
+  
+  accepts_nested_attributes_for :jobs, allow_destroy: true, 
+                reject_if: proc { |a| (a['square'].blank? || a['line_id'] == '1')}
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,8 @@ class Job < ActiveRecord::Base
   belongs_to :date_job
   belongs_to :line
   belongs_to :street
+
+  validates :line_id,   presence: true
+  validates :street_id, presence: true
+  validates :square,    presence: true, numericality: {greater_than_or_equal_to: 0.00}
 end
