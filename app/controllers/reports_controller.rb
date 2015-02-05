c28fd5b61a1ac3dd0c5bbbbde36319d75cc5f431
SergiiMelakh <msvkharkov@gmail.com>
2015-02-05 10:47:59 +0200

    Edit Controller and View for Filter and Report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,11 +7,14 @@ class ReportsController < ApplicationController
     @date_start = Date.civil(params[:date_start][:year].to_i, params[:date_start][:month].to_i, params[:date_start][:day].to_i)
     @date_end = Date.civil(params[:date_end][:year].to_i, params[:date_end][:month].to_i, params[:date_end][:day].to_i)
     @jobs_period = []
+    @sum_marking = Hash.new
     
       if params[:filter][:street_id] == "1"
         DateJob.where("date >= ? AND date <= ?", @date_start, @date_end).find_each do |dj|
-          dj.jobs.each do |j|
-           @jobs_period << j 
+            dj.jobs.each do |j|
+             @jobs_period << j 
+             marking = {"#{j.line.name}" => "#{j.square}".to_f}
+             @sum_marking.merge!(marking){|key, v1, v2| (v1+v2) }             
           end
          end
 
@@ -20,11 +23,13 @@ class ReportsController < ApplicationController
           dj.jobs.each do |j|
             if j.street_id == params[:filter][:street_id].to_i
               @jobs_period << j 
+              marking = {"#{j.line.name}" => "#{j.square}".to_f}
+              @sum_marking.merge!(marking){|key, v1, v2| v1+v2}
             end
           end
          end
       end
-
+      @sum_all_marking = @sum_marking.values.sum
 
   end
 end
