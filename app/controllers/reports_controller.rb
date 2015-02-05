0d1712ae73199b5cf6916dafa5d412aed48a2eaf
SergiiMelakh <msvkharkov@gmail.com>
2015-02-05 11:23:29 +0200

    Add sorting for line in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < ApplicationController
   def index
     @date_start = Date.civil(params[:date_start][:year].to_i, params[:date_start][:month].to_i, params[:date_start][:day].to_i)
     @date_end = Date.civil(params[:date_end][:year].to_i, params[:date_end][:month].to_i, params[:date_end][:day].to_i)
-    @jobs_period = []
+    @jobs_period = Array.new
     @sum_marking = Hash.new
     
       if params[:filter][:street_id] == "1"
@@ -14,7 +14,7 @@ class ReportsController < ApplicationController
             dj.jobs.each do |j|
              @jobs_period << j 
              marking = {"#{j.line.name}" => "#{j.square}".to_f}
-             @sum_marking.merge!(marking){|key, v1, v2| (v1+v2) }             
+             @sum_marking.merge!(marking){|key, v1, v2| (v1+v2) }            
           end
          end
 
@@ -24,7 +24,7 @@ class ReportsController < ApplicationController
             if j.street_id == params[:filter][:street_id].to_i
               @jobs_period << j 
               marking = {"#{j.line.name}" => "#{j.square}".to_f}
-              @sum_marking.merge!(marking){|key, v1, v2| v1+v2}
+              @sum_marking.merge!(marking){|key, v1, v2| v1+v2}              
             end
           end
          end
