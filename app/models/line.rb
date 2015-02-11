f368f50805216c6b915109679f44e0712cde7cd8
SergiiMelakh <msvkharkov@gmail.com>
2015-02-11 14:50:35 +0200

    Edit models

--- app/models/line.rb
+++ app/models/line.rb
@@ -2,5 +2,5 @@ class Line < ActiveRecord::Base
 
   has_many :jobs, dependent: :destroy 
   has_many :act_jobs, dependent: :destroy
-  #validates :name, presence: true
+  validates :name, presence: true
 end
