31af0de12904ac7dc13f6107e7aa29446773d410
SergiiMelakh <msvkharkov@gmail.com>
2015-02-26 14:48:39 +0200

    Edit views and add datepicker to filter

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,8 +4,8 @@ class ReportsController < ApplicationController
   end
 
   def index
-    @date_start = Date.civil(params[:date_start][:year].to_i, params[:date_start][:month].to_i, params[:date_start][:day].to_i)
-    @date_end = Date.civil(params[:date_end][:year].to_i, params[:date_end][:month].to_i, params[:date_end][:day].to_i)
+    @date_start = params[:filter][:date_start]
+    @date_end = params[:filter][:date_end]
     @jobs_period = Array.new
     @sum_marking = Hash.new
     
